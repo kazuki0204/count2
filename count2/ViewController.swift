4a53f1c952a60a54c1294c1f8c31f9bf5c346b61
Kazuki Marui <ka_ki0204@icloud.com>
2021-05-16 15:20:51 +0900

    countアプリ（チェック用）

--- count2/ViewController.swift
+++ count2/ViewController.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    
+    @IBOutlet var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func plus(){
+         number = number + 1
+        countLabel.text = String(number)
+        
+        judge()
+    }
+    @IBAction func minus(){
+        number = number - 1
+        countLabel.text = String(number)
+        
+        judge()
+    }
+    
+    @IBAction func times(){
+        number = number * 2
+        countLabel.text = String(number)
+        
+        judge()
+    }
+    
+    @IBAction func devided(){
+        number = number / 2
+        countLabel.text = String(number)
+        
+        judge()
+    }
+    
+    func judge(){
+        if number >= 10{
+            countLabel.textColor = UIColor.red
+        } else if number <= -10{
+            countLabel.textColor = UIColor.blue
+        } else {
+            countLabel.textColor = UIColor.black
+        }
+            
     }
 
 
